7a414e6bbda918f409267c585042b8cd2c259db9
Edgar Quezada <edgar.quezada@gmail.com>
2024-10-19 10:48:06 +0200

    fix typo in Board.js file

--- src/components/Board/Board.js
+++ src/components/Board/Board.js
@@ -2,7 +2,7 @@ import './Board.css'
 
 const Board = () => {
     const ranks = Array(8).fill().map((x,i) => 8-i)
-    const files = Array(8).fill().map((x,i) => String.fromCharCode)
+    const files = Array(8).fill().map((x,i) => String.fromCharCode[i + 97])
 
     return <div className='board'>
 
