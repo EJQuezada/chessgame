99de4e98dd97b72ddd301dfc15005e9cfceb54f9
Edgar Quezada <edgar.quezada@gmail.com>
2024-10-19 11:02:29 +0200

    update helper.js and Board.js files

--- src/components/Board/Board.js
+++ src/components/Board/Board.js
@@ -1,8 +1,9 @@
+import { getCharacter } from '../../helper'
 import './Board.css'
 
 const Board = () => {
     const ranks = Array(8).fill().map((x,i) => 8-i)
-    const files = Array(8).fill().map((x,i) => String.fromCharCode[i + 97])
+    const files = Array(8).fill().map((x,i) => getCharacter(i))
 
     return <div className='board'>
 
