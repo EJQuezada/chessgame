4362c848858babf6dc8147a3c61961a3e0d31c42
Edgar Quezada <edgar.quezada@gmail.com>
2024-10-22 19:31:52 +0200

    update Pieces.css and Piece.js files

--- src/components/Pieces/Pieces.js
+++ src/components/Pieces/Pieces.js
@@ -5,7 +5,11 @@ const Pieces = () => {
     const position = new Array(8).fill('').map(x=> new Array(8).fill(''))
 
     position[0][0] = 'wr'
+    position[1][7] = 'wn'
+    position[0][0] = 'wq'
+    position[0][0] = 'wb'
     position[7][7] = 'br'
+    position[7][7] = 'bn'
     
     console.log(position);
 
