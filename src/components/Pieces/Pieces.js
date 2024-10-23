1de8ba4c19931af1b6c08364d92ab00a41bcba8c
Edgar Quezada <edgar.quezada@gmail.com>
2024-10-23 15:06:35 +0200

    update Pieces.js

--- src/components/Pieces/Pieces.js
+++ src/components/Pieces/Pieces.js
@@ -1,19 +1,32 @@
 import './Pieces.css'
 import Piece from './Piece'
-import { useState } from 'react'
+import { useState, useRef } from 'react'
 import { createPosition } from '../../helper'
 
 const Pieces = () => {
 
+    const ref = useRef()
+
     const [state,setState] = useState(createPosition())
     
+    const calculateCoords = e => {
+        const {width, left, top} = ref.current.getBoundingClientRect()
+        const size = width / 8
+        const y = Math.floor((e.clientX - left) / size)
+        const x = 7- Math.floor((e.clientY - top) / size)
+        return (x,y)
+    }
+
     const onDrop = e => {
+        const {x,y} = calculateCoords(e)
+
         const [p,rank,file] = e.dataTransfer.getData('text').split(',')
-        console.log(p,rank,file);
+        
     }
     const onDragOver = e => e.preventDefault()
     
     return <div
+        ref={ref}
         onDrop={onDrop}
         onDragOver={onDragOver}
         className='pieces'>
