2701d6e52e593f4574faa5ba841cbe6be898094e
Edgar Quezada <edgar.quezada@gmail.com>
2024-10-25 18:24:25 +0200

    update syntax in getMoves.js

--- src/arbiter/getMoves.js
+++ src/arbiter/getMoves.js
@@ -1,6 +1,6 @@
 import arbiter from "./arbiter"
 
-export const getRookMoves = (position,piece,rank,file) => {
+export const getRookMoves = ({position,piece,rank,file}) => {
     const moves = []
     const us = piece[0]
     const enemy = us === 'w' ? 'b' : 'w'
