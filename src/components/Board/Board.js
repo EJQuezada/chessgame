fb9c11b514bf2441cbbfb2a438042203d16e9283
Edgar Quezada <edgar.quezada@gmail.com>
2024-10-25 15:32:39 +0200

    update Board.js with attack and highlight functions

--- src/components/Board/Board.js
+++ src/components/Board/Board.js
@@ -1,19 +1,30 @@
-//import { getCharacter } from '../../helper'
-import Pieces from '../Pieces/Pieces'
 import './Board.css'
-import Files from './bits/Files'
+
 import Ranks from './bits/Ranks'
+import Files from './bits/Files'
+import Pieces from '../Pieces/Pieces'
+import { useAppContext } from '../../contexts/Context'
 
 const Board = () => {
-
-    const getClassName = (i,j) => {
-       let c = 'tile'
-       c+= (i+j)%2 === 0 ? ' tile--dark' : ' tile--light'
-       return c
-    }
-
     const ranks = Array(8).fill().map((x,i) => 8-i)
     const files = Array(8).fill().map((x,i) => i+1)
+
+    const {appState} = useAppContext()
+    const position = appState.position[appState.position.length -1]
+    
+    const getClassName = (i,j) => {
+       let c = 'tile'
+       c+= (i+j)%2 === 0 ? ' tile--dark' : ' tile--light '
+
+        if (appState.candidateMoves?.find(m => m[0] === i && m[1] === j)){
+            if(position[i][j])
+                c+= ' attacking'
+            else
+                c+= ' highlight'
+        }
+
+       return c
+    }
 
     return <div className='board'>
 
