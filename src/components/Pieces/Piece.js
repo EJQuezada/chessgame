a5aa659e4edb05d623e5ea5031ae2096a1240842
Edgar Quezada <edgar.quezada@gmail.com>
2024-10-25 12:30:15 +0200

    add candidateMoves logic to Piece.js and arbiter.js

--- src/components/Pieces/Piece.js
+++ src/components/Pieces/Piece.js
@@ -1,3 +1,4 @@
+import { useAppContext } from "../../contexts/Context"
 
 const Piece = ({
     rank, 
@@ -5,6 +6,41 @@ const Piece = ({
     piece, 
 }) => {
 
+    const {appState, dispatch} = useAppContext()
+    const {turn, position} = appState;
+    const currentPosition = position[position.length - 1]
+
+    const getMoves = () => {
+        const moves = []
+        const us = piece[0]
+        const enemy = us === 'w' ? 'b' : 'w'
+
+        const direction = [
+            [-1,0],
+            [1,0],
+            [0,-1],
+            [0,1],
+        ]
+
+        direction.forEach(dir => {
+            for (let i = 1; i < 8; i++) {
+                const x = rank + (i*dir[0])
+                const y = file + (i*dir[1])
+                if (currentPosition?.[x]?.[y] === undefined)
+                    break
+                if (currentPosition[x][y].startWith(enemy)){
+                    moves.push([x,y])
+                    break
+                }
+                if (currentPosition[x][y].startWith(us))
+                    break
+
+                moves.push([x,y])                
+            }
+        })
+
+        return moves
+    }
 
     const onDragStart = e => {
         e.dataTransfer.effectAllowed = 'move'
@@ -12,9 +48,14 @@ const Piece = ({
         setTimeout(() => {
             e.target.style.display = 'none'
         },0)
+        if (turn === piece[0]) {
+            const candidateMoves = getMoves()
+            console.log(candidateMoves);
+        }
     }
-
-    const onDragEnd = e => e.target.style.display = 'block'
+    const onDragEnd = e => {
+        e.target.style.display = 'block'
+    }
 
     return (
         <div 
@@ -22,6 +63,7 @@ const Piece = ({
             draggable={true}
             onDragEnd={onDragEnd}
             onDragStart={onDragStart}
+
         />
     )
 } 
