425946dde8b7ca9439b5187df07b38b4b5be231f
Edgar Quezada <edgar.quezada@gmail.com>
2024-10-28 12:21:47 +0100

    update getMoves.js file with pawn moves

--- src/arbiter/getMoves.js
+++ src/arbiter/getMoves.js
@@ -118,6 +118,11 @@ export const getPawnMoves = ({position,piece,rank,file}) => {
     if (!position?.[rank+dir][file]){
         moves.push([rank+dir,file])
     }
+    
+    if (rank % 5 === 1){
+        if (!position?.[rank+dir]?.[file] === '' && position?.[rank+dir+dir]?.[file] ===){
+            moves.push([rank+dir+dir,file])
+        }
 
     return moves
 }
