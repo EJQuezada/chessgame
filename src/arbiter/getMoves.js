80c3d36b6a16787080232952198c97b2780f876d
Edgar Quezada <edgar.quezada@gmail.com>
2024-10-28 09:12:36 +0100

    update rank moves

--- src/arbiter/getMoves.js
+++ src/arbiter/getMoves.js
@@ -49,7 +49,7 @@ export const getKnightMoves = ({position,rank,file}) => {
     candidates.forEach(c =>{
         const cell = position[rank+c[0]]?.[file+c[1]] 
         if (cell !== undefined && (cell.startsWith(enemy) || cell === ''))
-            moves.push([[rank+c[0]],[file+c[1]]] )
+            moves.push([rank+c[0],file+c[1]] )
     })
     return moves
 }
