8e862c5f765155e1adebf0155184e07b0a816bed
Edgar Quezada <edgar.quezada@gmail.com>
2024-10-28 09:26:29 +0100

    add getBishopMoves functions to getMoves.js file

--- src/arbiter/getMoves.js
+++ src/arbiter/getMoves.js
@@ -35,7 +35,6 @@ export const getRookMoves = ({position,piece,rank,file}) => {
 export const getKnightMoves = ({position,rank,file}) => {
     const moves = []
     const enemy = position[rank][file].startsWith('w') ? 'b' : 'w'
-
     const candidates = [
         [-2,-1],
         [-2,1],
@@ -51,5 +50,37 @@ export const getKnightMoves = ({position,rank,file}) => {
         if (cell !== undefined && (cell.startsWith(enemy) || cell === ''))
             moves.push([rank+c[0],file+c[1]] )
     })
+    return moves
+}
+
+export const getBishopMoves = ({position,piece,rank,file}) => {
+    const moves = []
+    const us = piece[0]
+    const enemy = us === 'w' ? 'b' : 'w'
+
+    const direction = [
+        [-1,-1],
+        [-1,1],
+        [1,-1],
+        [1,1],
+    ]
+
+    direction.forEach(dir => {
+        for (let i = 1; i < 8; i++) {
+            const x = rank + (i*dir[0])
+            const y = file + (i*dir[1])
+            if (position?.[x]?.[y] === undefined)
+                break
+            if (position[x][y].startsWith(enemy)){
+                moves.push ([x,y])
+                break;
+            }
+            if (position[x][y].startsWith(us)){
+                break
+            }
+            moves.push([x,y])
+        }
+    })
+
     return moves
 }
