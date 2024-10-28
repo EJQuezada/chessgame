e6f0498cde68299fb7ae8077cef397be6ff6ee24
Edgar Quezada <edgar.quezada@gmail.com>
2024-10-28 12:27:25 +0100

    update getPawnMoves function for first and subsequent moves

--- src/arbiter/getMoves.js
+++ src/arbiter/getMoves.js
@@ -115,14 +115,17 @@ export const getPawnMoves = ({position,piece,rank,file}) => {
     const moves = []
     const dir = piece === 'wp' ? 1 : -1
 
-    if (!position?.[rank+dir][file]){
-        moves.push([rank+dir,file])
-    }
-    
+    //Move two tiles on first move
     if (rank % 5 === 1){
         if (!position?.[rank+dir]?.[file] === '' && position?.[rank+dir+dir]?.[file] === ''){
             moves.push([rank+dir+dir,file])
         }
     }
+    
+    //Move one tile subsequently
+    if (!position?.[rank+dir]?.[file]){
+        moves.push([rank+dir,file])
+    }
+    
     return moves
 }
