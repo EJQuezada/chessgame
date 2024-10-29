06e5f95ae4cd0b056105e7ad8a71cc5a9446fc6d
Edgar Quezada <edgar.quezada@gmail.com>
2024-10-29 15:16:30 +0100

    update getMoves.js and Piece.js

--- src/arbiter/getMoves.js
+++ src/arbiter/getMoves.js
@@ -1,4 +1,4 @@
-import arbiter from './arbiter'
+import arbiter from "./arbiter"
 
 export const getRookMoves = ({position,piece,rank,file}) => {
     const moves = []
@@ -13,7 +13,7 @@ export const getRookMoves = ({position,piece,rank,file}) => {
     ]
 
     direction.forEach(dir => {
-        for (let i = 1; i < 8; i++) {
+        for (let i = 1; i <= 8; i++) {
             const x = rank + (i*dir[0])
             const y = file + (i*dir[1])
             if (position?.[x]?.[y] === undefined)
@@ -25,7 +25,7 @@ export const getRookMoves = ({position,piece,rank,file}) => {
             if (position[x][y].startsWith(us)){
                 break
             }
-            moves.push([x,y])
+            moves.push ([x,y])
         }
     })
 
@@ -47,7 +47,7 @@ export const getKnightMoves = ({position,rank,file}) => {
         [2,1],
     ]
     candidates.forEach(c =>{
-        const cell = position[rank+c[0]]?.[file+c[1]] 
+        const cell = position?.[rank+c[0]]?.[file+c[1]] 
         if (cell !== undefined && (cell.startsWith(enemy) || cell === '')){
             moves.push([rank+c[0],file+c[1]] )
         }
@@ -68,7 +68,7 @@ export const getBishopMoves = ({position,piece,rank,file}) => {
     ]
 
     direction.forEach(dir => {
-        for (let i = 1; i < 8; i++) {
+        for (let i = 1; i <= 8; i++) {
             const x = rank + (i*dir[0])
             const y = file + (i*dir[1])
             if (position?.[x]?.[y] === undefined)
